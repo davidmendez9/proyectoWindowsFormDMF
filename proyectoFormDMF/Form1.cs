53c1037164b4237b3e63464510493c2a3b3ca5d0
davidmendez9 <david.mf2004@gmail.com>
2023-11-14 16:21:43 +0100

    first

---
+++ proyectoFormDMF/Form1.cs
@@ -0,0 +1,10 @@
+namespace proyectoFormDMF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ proyectoFormDMF/ImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoFormDMF
+{
+    internal class ImageConverter
+    {
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
